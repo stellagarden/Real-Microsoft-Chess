897e0dc054aba22b2f988ecf370b5c3e4e8402bd
Hyemin Stella Lee <hlee3045@gatech.edu>
2023-01-17 14:53:11 -0500

    code for testing cds sensors

---
+++ test_cde_value.ino
@@ -0,0 +1,13 @@
+void setup() {  
+  Serial.begin(9600);
+}
+
+void loop() {
+  for(int i=0;i<16;i++){
+    Serial.print(i);
+    Serial.print(" : ");
+    Serial.println(analogRead(i));
+  }
+  delay(10);
+}
+
